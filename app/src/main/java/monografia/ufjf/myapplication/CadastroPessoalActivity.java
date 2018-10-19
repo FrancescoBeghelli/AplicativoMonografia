d20c073d60921910e14ba5b8ffaa95b219d556cb
FrancescoBeghelli <francesco_jf@hotmail.com>
2018-10-19 09:28:51 -0300

    Criação Layout Cadastro Pessoal

--- app/src/main/java/monografia/ufjf/myapplication/CadastroPessoalActivity.java
+++ app/src/main/java/monografia/ufjf/myapplication/CadastroPessoalActivity.java
@@ -2,12 +2,27 @@ package monografia.ufjf.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroPessoalActivity extends AppCompatActivity {
+
+    private Button btnCadastroPessoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_pessoal);
+
+        btnCadastroPessoal = (Button) findViewById(R.id.btnCadastroPessoal);
+
+        btnCadastroPessoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                finish();
+            }
+        });
     }
 }
